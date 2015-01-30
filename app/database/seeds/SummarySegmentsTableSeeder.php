6a5edfdc1cf9dc780922cfb12290cc020c7aa608
D3v1an <d3v1an.tux@gmail.com>
2015-01-30 06:06:19 -0600

    Fix de seeder

--- app/database/seeds/SummarySegmentsTableSeeder.php
+++ app/database/seeds/SummarySegmentsTableSeeder.php
@@ -9,23 +9,20 @@ class SummarySegmentsTableSeeder extends Seeder {
 	{
 		$faker = Faker::create();
 
-		foreach(range(1, 10) as $index)
-		{
-			Segment::create(['board_id'=>3,'name'=>'Principal']);
-			Segment::create(['board_id'=>3,'name'=>'Estados']);
-			Segment::create(['board_id'=>3,'name'=>'Subsecretarias']);
-			Segment::create(['board_id'=>3,'name'=>'Primeras Planas']);
-			Segment::create(['board_id'=>3,'name'=>'Temas varios']);
-			Segment::create(['board_id'=>3,'name'=>'Columnas Politicas']);
-			Segment::create(['board_id'=>3,'name'=>'Cartones']);
-			Segment::create(['board_id'=>51,'name'=>'Notas Principales EPN']);
-			Segment::create(['board_id'=>51,'name'=>'Pemex en medios nacionales']);
-			Segment::create(['board_id'=>51,'name'=>'En los estados']);
-			Segment::create(['board_id'=>51,'name'=>'Primeras Planas']);
-			Segment::create(['board_id'=>51,'name'=>'Columnas Politicas']);
-			Segment::create(['board_id'=>51,'name'=>'Columnas Financieras']);
-			Segment::create(['board_id'=>51,'name'=>'Cartones']);
-		}
+		Segment::create(['board_id'=>3,'name'=>'Principal']);
+		Segment::create(['board_id'=>3,'name'=>'Estados']);
+		Segment::create(['board_id'=>3,'name'=>'Subsecretarias']);
+		Segment::create(['board_id'=>3,'name'=>'Primeras Planas']);
+		Segment::create(['board_id'=>3,'name'=>'Temas varios']);
+		Segment::create(['board_id'=>3,'name'=>'Columnas Politicas']);
+		Segment::create(['board_id'=>3,'name'=>'Cartones']);
+		Segment::create(['board_id'=>51,'name'=>'Notas Principales EPN']);
+		Segment::create(['board_id'=>51,'name'=>'Pemex en medios nacionales']);
+		Segment::create(['board_id'=>51,'name'=>'En los estados']);
+		Segment::create(['board_id'=>51,'name'=>'Primeras Planas']);
+		Segment::create(['board_id'=>51,'name'=>'Columnas Politicas']);
+		Segment::create(['board_id'=>51,'name'=>'Columnas Financieras']);
+		Segment::create(['board_id'=>51,'name'=>'Cartones']);
 	}
 
 }
